19b0b2749c25b6fd0b4878e51c557b0a31ae90aa
Andrei-Biareshchanka <a.biareshchanka@gmail.com>
2024-07-24 15:57:09 +0200

    feat: add constants and change Game component

---
+++ src/components/game/Game.jsx
@@ -0,0 +1,110 @@
+'use client';
+
+import { useReducer } from 'react';
+import GameLayout from '@/components/game/ui/GameLayout' 
+import GameTitle from '@/components/game/ui/GameTitle'
+import GameInfo from '@/components/game/ui/GameInfo'
+import GameMoveInfo from '@/components/game/ui/GameMoveInfo'
+import GameCell from '@/components/game/ui/GameCell'
+import GameOverModal from '@/components/game/ui/GameOverModal'
+import BackLink from '@/components/game/ui/BackLink'
+import PlayerInfo from '@/components/game/ui/PlayerInfo'
+import { PLAYERS } from '@/components/game/constants'
+import { 
+  GAME_STATE_ACTIONS, 
+  gameReducer, 
+  initGameState,
+} from '@/components/game/model/gameReducer'
+import { computeWinnerSymbol } from '@/components/game/model/computeWinnerSymbol';
+import { computeWinner } from '@/components/game/model/computeWinner';
+import { getNextMove } from '@/components/game/model/getNextMove';
+import { computePlayerTimer } from '@/components/game/model/computePlayerTimer';
+import { useInterval } from '../lib/timers';
+
+const PLAYERS_COUNT = 4;
+
+export default function Game() {
+  const [gameState, dispatch] = useReducer(
+    gameReducer, 
+    { 
+      playersCount: PLAYERS_COUNT, 
+      currentMoveStart: Date.now(),
+      defaultTimer: 10000,
+    }, 
+    initGameState
+  );
+
+  const { cells, currentMove, timers } = gameState;
+
+  useInterval(1000, currentMove, () => {
+    dispatch({ 
+      type: GAME_STATE_ACTIONS.TICK,
+      now: Date.now(),
+     });
+  })
+
+  const winnerSequence = computeWinner(gameState);
+  const nextMove = getNextMove(gameState);
+  const winnerSymbol = computeWinnerSymbol(gameState, { 
+    winnerSequence, 
+    nextMove,
+  });
+
+  const winnerPlayer = PLAYERS.find(player => player.symbol === winnerSymbol);
+
+  return (
+    <>
+    <GameLayout 
+      title={<GameTitle />} 
+      gameInfo={<GameInfo playersCount={4} isRatingGame timeMode="1 min per move" />} 
+      backLink={<BackLink />}
+      playersList={
+        PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => {
+          const { timer, timerStartAt } = computePlayerTimer(gameState, player.symbol);
+
+          return (
+            <PlayerInfo 
+              key={player.id} 
+              avatar={player.avatar} 
+              name={player.name} 
+              rating={player.rating} 
+              symbol={player.symbol}
+              isRight={index % 2 === 1}
+              timer={timer}
+              timerStartAt={timerStartAt}
+            />
+          )
+        }
+        )
+      }
+      gameMoveInfo={<GameMoveInfo currentMove={currentMove} nextMove={nextMove} />}
+      gameCells={cells.map((cell, index) => (
+        <GameCell key={index}
+          isWinner={winnerSequence?.includes(index)}
+          onClick={() => dispatch(
+            { 
+              type: GAME_STATE_ACTIONS.CELL_CLICK, 
+              index,
+              now: Date.now(),
+            }
+          )}
+          disabled={!!winnerSymbol} 
+          symbol={cell}
+        />))}
+    />
+    <GameOverModal 
+      winnerName={winnerPlayer?.name}
+      players={PLAYERS.slice(0, PLAYERS_COUNT).map((player, index) => (
+        <PlayerInfo 
+          key={player.id} 
+          avatar={player.avatar} 
+          name={player.name} 
+          rating={player.rating} 
+          symbol={player.symbol}
+          timer={timers[player.symbol]}
+          isRight={index % 2 === 1}
+        />))}        
+      />
+    </>
+  )
+}
